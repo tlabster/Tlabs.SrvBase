13bb7117d5d91c7b81d9cac056c459ce9014d9af
Christian Jäckel <Christian Jckel@POL>
2019-01-24 17:33:05 +0100

    Proxy tests

---
+++ tst/ProxyTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Newtonsoft.Json.Linq;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tlabs.Middleware.Proxy.Test {
+
+  public class TestController : Controller {
+
+    [HttpGet("V3/BasketPoint/Service/BasketPoint.svc/BasketPoint")]
+    public ActionResult GetBasketPoints() {
+      Console.WriteLine("MVC controller: V3/BasketPoint/Service/BasketPoint.svc/BasketPoint");
+
+      if (null == HttpContext.GetProxyRequestMessage())
+        throw new InvalidOperationException("No 'proxyMsg' set with HttpContext");
+      if (null == HttpContext.GetProxyResponseMessage())
+        throw new InvalidOperationException("No 'proxyResp' set with HttpContext");
+
+      return StatusCode(418, "Teapot response from basket controller");
+    }
+
+    [HttpGet("api/{*all}")]
+    public ActionResult GeneralAPI() {
+      return Ok("API");
+    }
+  }
+
+  public class ProxyTest : IClassFixture<ProxyTest.ProxyTestFixture> {
+
+    public class ProxyTestFixture {
+        public readonly TestServer Server;
+        public readonly HttpClient Client;
+
+      public ProxyTestFixture() {
+        this.Server= new TestServer(new WebHostBuilder().UseStartup<ProxyTest.Startup>());
+        this.Client= this.Server.CreateClient();
+      }
+    }
+
+    public class Startup {
+      public void ConfigureServices(IServiceCollection services) {
+        services.AddRouting();
+        services.AddMvc();
+        services.AddSingleton<HttpClient>(); //(svcProv =>
+      }
+
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        app.UseMvcProxy(new ProxyEndpoint[] {
+          new ProxyEndpoint {
+            EndpointTemplate= "V{svcVers}/{svcCat}/Service/{svcName}.svc/{svcEnd}",
+            ProxyUriBuilder= (context, args) => $"https://sandbox.prime-cloud.com/V{args["svcVers"]}/{args["svcCat"]}/Service/{args["svcName"]}.svc/{args["svcEnd"]}"
+          }
+        });
+
+#if false
+        app.UseProxy("api/comments/contextandargstotask/{postId}", (context, args) => {
+          context.GetHashCode();
+          return $"https://jsonplaceholder.typicode.com/comments/{args["postId"]}";
+        });
+
+        app.UseProxy("api/comments/argstotask/{postId}", (args) => {
+          return $"https://jsonplaceholder.typicode.com/comments/{args["postId"]}";
+        });
+
+        app.UseProxy("api/comments/emptytotask", () => {
+          return $"https://jsonplaceholder.typicode.com/comments/1";
+        });
+#endif
+
+        app.UseMvc();
+
+      }
+    }
+
+    public static class ProxyRouteMethods {
+
+      [ProxyRoute("api/posts/tostring/{postId}")]
+      public static string ProxyToString(int postId) {
+        return $"https://jsonplaceholder.typicode.com/posts/{postId}";
+      }
+
+      //[ProxyRoute("http://int.prime-cloud.com/V3/BasketPoint/Service/BasketPoint.svc/BasketPoint")]
+      [ProxyRoute("V{svcVers}/{svcCat}/Service/{svcName}.svc/{svcEnd}")]
+      public static string ProxyToPcApi(int svcVers, string svcCat, string svcName, string svcEnd) {
+        return $"http://int.prime-cloud.com/V{svcVers}/{svcCat}/Service/{svcName}.svc/{svcEnd}";
+      }
+    }
+
+
+    private ProxyTestFixture testFixture;
+    private readonly ITestOutputHelper tstout;
+
+    public ProxyTest(ProxyTestFixture testFixture, ITestOutputHelper output) {
+      this.testFixture= testFixture;
+      this.tstout= output;
+    }
+
+
+    [Fact]
+    public async Task SimpleApi() {
+      var response= await testFixture.Client.GetAsync("api/ok/check/this"); ///ok/check/this
+      Assert.True(response.IsSuccessStatusCode);
+      Assert.StartsWith("text/", response.Content.Headers.ContentType.MediaType);
+
+      var respStr= await response.Content.ReadAsStringAsync();
+      Assert.Equal("API", respStr);
+    }
+
+    [Fact]
+    public async Task ProxyBasketApi() {
+      var response= await testFixture.Client.GetAsync("V3/BasketPoint/Service/BasketPoint.svc/BasketPoint");
+      Assert.Equal(418, (int)response.StatusCode);  //handled by TestController.GetBasketPoints() ??
+
+      var respStr= await response.Content.ReadAsStringAsync();
+      Assert.Contains("Teapot response", respStr);
+    }
+
+    [Fact]
+    public async Task ProxyEarnApi() {
+      var response= await testFixture.Client.GetAsync("V3/Earn/Service/Earn.svc/Earn");
+      Assert.Equal(400, (int)response.StatusCode);
+
+      //check for some PrimeCloud specific response properties:
+      Assert.Equal("text/html", response.Content.Headers.ContentType.MediaType);
+      Assert.StartsWith("Microsoft-IIS", response.Headers.Server.ToString());
+      Assert.True(response.Headers.Contains("X-AspNet-Version"));
+
+      var respStr= await response.Content.ReadAsStringAsync();
+      Assert.Contains("<title>Request Error</title>", respStr);
+    }
+  }
+}
