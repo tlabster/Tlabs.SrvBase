50f84a7cc60c477b9fed0d233f09dca043a3f4df
Christian Jäckel <c.jaeckel@tomorrowlabs.io>
2019-11-18 15:31:35 +0100

    Auth filter

---
+++ src/Auth/ApiKeyAuthorizationFilter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using Tlabs.Config;
+
+namespace Tlabs.Server.Auth {
+  ///<summary>Authorization filter to check for a configured API key.</summary>
+  // https://randomkeygen.com/
+  //256 bit: CIHgErMCjsAhfGvS8J5VHX3ZwInMGYgX
+  public class ApiKeyAuthorizationFilter : IAsyncAuthorizationFilter {
+    static readonly ILogger log= Tlabs.App.Logger<ApiKeyAuthorizationFilter>();
+    const string HEADER_AUTH_KEY= "Authorization";
+    Options authOptions;
+    Regex pathPattern;
+    ///<summary>Ctor from <paramref name="options"/>.</summary>
+    public ApiKeyAuthorizationFilter(IOptions<Options> options) {
+      this.authOptions= options.Value;
+      this.pathPattern= new Regex(authOptions.pathPolicy, RegexOptions.Compiled);
+    }
+    ///<inheritdoc/>
+    public Task OnAuthorizationAsync(AuthorizationFilterContext ctx) {
+      // Skip filter if header does not contain an api key or action is marked as anonymous
+      if (ctx.Filters.Any(item => item is IAllowAnonymousFilter)) { return Task.CompletedTask; }
+      if (!ctx.HttpContext.Request.Headers.ContainsKey(HEADER_AUTH_KEY)) { return Task.CompletedTask; }
+
+      var route= ctx.ActionDescriptor.AttributeRouteInfo.Template;
+      string key= null;
+      var authorize= ctx.HttpContext.Request.Headers[HEADER_AUTH_KEY];
+
+      if (1 == authorize.Count) {
+        var authParts= authorize[0].Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+        if (2 == authParts.Length && 0 == string.Compare(authParts[0].Trim(), "ApiKey", StringComparison.InvariantCultureIgnoreCase))
+          key= authParts[1];
+      }
+      if (   pathPattern.IsMatch(route)
+          && null == key || key != authOptions.masterKey) {
+        log.LogInformation("Unauthorized access: {path}", ctx.HttpContext.Request.Path);
+
+        var err= new JsonResult(new {
+          success= false,
+          error= "Unauthorized Request"
+        });
+        err.StatusCode= StatusCodes.Status401Unauthorized;
+        ctx.Result= err;  //setting a result does short-circuit the remainder of the filter pipeline...
+      }
+      return Task.CompletedTask;
+    }
+
+    ///<summary>Filter options.</summary>
+    public class Options {
+      ///<summary>Path policy regex pattern.</summary>
+      public string pathPolicy { get; set; }
+      ///<summary>Master API key.</summary>
+      ///<remarks>Clear text (should not be used)</remarks>
+      public string masterKey { get; set; }
+    }
+    /// <summary>Configurator</summary>
+    public class Configurator : IConfigurator<IServiceCollection> {
+      /// <inheritdoc/>
+      public void AddTo(IServiceCollection svcColl, IConfiguration cfg) {
+        svcColl.Configure<Options>(cfg.GetSection("config"));
+        svcColl.AddSingleton<ApiKeyAuthorizationFilter>();
+        ApiKeyAuthorizationFilter.log.LogInformation("Service {s} added.", nameof(ApiKeyAuthorizationFilter));
+      }
+    }
+
+  }
+}
---
+++ src/Auth/AuthCookieAuthorizationFilter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Tlabs.Config;
+using Tlabs.Data.Entity;
+
+namespace Tlabs.Server.Auth {
+  /// <summary>Enforces the role authorization</summary>
+  public class AuthCookieAuthorizationFilter : AuthorizeFilter {
+    static readonly ILogger log= Tlabs.App.Logger<AuthCookieAuthorizationFilter>();
+
+    /// <summary>Default ctor that requires auhtenticated user policy.</summary>
+    public AuthCookieAuthorizationFilter() : base(new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build()) { }
+    /// <inheritoc/>
+    public AuthCookieAuthorizationFilter(AuthorizationPolicy policy) : base(policy) { }
+
+    /// <inheritoc/>
+    public AuthCookieAuthorizationFilter(IEnumerable<IAuthorizeData> authorizeData) : base(authorizeData) { }
+
+    /// <inheritoc/>
+    public AuthCookieAuthorizationFilter(string policy) : base(policy) { }
+
+    /// <inheritoc/>
+    public AuthCookieAuthorizationFilter(IAuthorizationPolicyProvider policyProvider, IEnumerable<IAuthorizeData> authorizeData) : base(policyProvider, authorizeData) { }
+
+    /// <inheritoc/>
+    public override async System.Threading.Tasks.Task OnAuthorizationAsync(AuthorizationFilterContext context) {
+      if(!context.HttpContext.Request.Headers["Authorization"].Any()) {
+        try {
+          await base.OnAuthorizationAsync(context);
+
+          var type= GetRole(context);
+
+          if (null == type) return;
+
+          var route= context.ActionDescriptor.AttributeRouteInfo.Template;
+
+          var allowedActions= Role.DefaultAuthorizedActions[type.Value];
+          var deniedActions= Role.DefaultDeniedActions[type.Value];
+
+          bool denied= null != deniedActions && deniedActions.IsMatch(route);
+          if (!denied && allowedActions.IsMatch(route)) return;
+
+          context.Result= new ForbidResult(CookieAuthenticationDefaults.AuthenticationScheme);
+        } catch (Exception e) {
+          log.LogCritical("Error in authorization process: ", e);
+          var err= new JsonResult(new {
+            success= false,
+            error= ""
+          });
+          err.StatusCode= StatusCodes.Status500InternalServerError;
+          context.Result= err;  //setting a result does short-circuit the remainder of the filter pipeline...
+        }
+      }
+    }
+
+    /// <summary>Gets the current role based on the context</summary>
+    public Role.RoleType? GetRole(AuthorizationFilterContext context) {
+      var idSrvc= (Tlabs.Identity.IIdentityAccessor)App.ServiceProv.GetService(typeof(Tlabs.Identity.IIdentityAccessor));
+
+      if (idSrvc==null) throw new ArgumentNullException(nameof(Tlabs.Identity.IIdentityAccessor));
+
+      string currentRole= idSrvc.Roles.FirstOrDefault();
+
+      if (currentRole==null) return null;
+
+      return (Role.RoleType)Enum.Parse(typeof(Role.RoleType), currentRole);
+    }
+
+    /// <summary>Configurator</summary>
+    public class Configurator : IConfigurator<IServiceCollection> {
+      /// <inheritoc/>
+      public void AddTo(IServiceCollection svcColl, IConfiguration cfg) {
+        var log= App.Logger<Configurator>();
+        svcColl.AddSingleton(new AuthCookieAuthorizationFilter());
+        log.LogInformation("Service {s} added.", nameof(AuthCookieAuthorizationFilter));
+      }
+    }
+  }
+}
